9d5d3a91a1e389820f78946129bd350876d8ca2c
Daniel Truemper <truemper@retresco.de>
2011-01-17 16:33:12 +0100

    Adding the Fetch processor
    refs #27
    
    This processor downloads content from the web using Tornados
    AsyncHTTPClient.

--- src/spyder/processor/fetcher.py
+++ src/spyder/processor/fetcher.py
@@ -27,54 +27,131 @@
 #
 #
 
-from zmq.eventloop.ioloop import IOLoop
-from zmq.eventloop.zmqstream import ZMQStream
+import logging
+
+from urlparse import urlsplit
 
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+from tornado.httputil import HTTPHeaders
 
-from spyder.core.worker import AsyncZmqWorker
+from zmq.eventloop.ioloop import IOLoop
+
+from spyder.core.constants import CURI_SITE_USERNAME
+from spyder.core.constants import CURI_SITE_PASSWORD
 from spyder.core.messages import DataMessage
+from spyder.core.messages import serialize_date_time, deserialize_date_time
+from spyder.core.worker import AsyncZmqWorker
 from spyder.thrift.gen.ttypes import CrawlUri
 
+LOG = logging.getLogger('fetcher')
 
-class Fetcher(object):
+
+class FetchProcessor(object):
     """
     A processing class for downloading all kinds of stuff from the web.
     """
 
-    def __init__(self, io_loop=None, max_clients=None,
-            max_simultaneous_connections=None):
+    def __init__(self, settings, io_loop=None):
         """
         Initialize the members.
         """
+        self._user_agent = settings.USER_AGENT
+        assert self._user_agent
+
         self._io_loop = io_loop or IOLoop.instance()
-        self._max_clients = max_clients or 10
-        self._max_simultaneous_connections = max_simultaneous_connections or 1
+        self._max_clients = settings.MAX_CLIENTS
+        self._max_simultaneous_connections = \
+            settings.MAX_SIMULTANEOUS_CONNECTIONS
+        self._follow_redirects = settings.FOLLOW_REDIRECTS
+        self._max_redirects = settings.MAX_REDIRECTS
+        self._gzip = settings.USE_GZIP
 
         self._client = AsyncHTTPClient(self._io_loop,
             max_clients=self._max_clients,
             max_simultaneous_connections=self._max_simultaneous_connections)
 
-        self._stream = ZMQStream(self._in_socket, self._io_loop)
-        self.start()
-
-    def __call__(self, raw_message, out_socket):
+    def __call__(self, msg, out_socket):
         """
-        We have a message.
-
-        Deserialize the crawl_uri and start working with it.
+        Work on the current `DataMessage` and send the result to `out_socket`.
         """
-        msg = DataMessage(raw_message)
+        # prepare the HTTPHeaders
+        headers = self._prepare_headers(msg)
 
-        request = HTTPRequest(msg.curi.url)
+        last_modified = None
+        if msg.curi.req_header:
+            # check if we have a date when the page was last crawled
+            if "Last-Modified" in msg.curi.req_header:
+                last_modified = deserialize_date_time(
+                        msg.curi.req_header["Last-Modified"])
+
+        # check if we have username and password present
+        auth_username = None
+        auth_password = None
+        if msg.curi.optional_vars and \
+            CURI_SITE_USERNAME in msg.curi.optional_vars and \
+            CURI_SITE_PASSWORD in msg.curi.optional_vars:
+
+            auth_username = msg.curi.optional_vars[CURI_SITE_USERNAME]
+            auth_password = msg.curi.optional_vars[CURI_SITE_PASSWORD]
+
+        # create the request
+        request = HTTPRequest(msg.curi.effective_url,
+                method = "GET",
+                headers = headers,
+                auth_username = auth_username,
+                auth_password = auth_password,
+                if_modified_since = last_modified,
+                follow_redirects = self._follow_redirects,
+                max_redirects = self._max_redirects,
+                user_agent = self._user_agent)
+
+        LOG.info("proc.fetch::request for %s" % msg.curi.url)
         self._client.fetch(request, self._handle_response(msg, out_socket))
+
+    def _prepare_headers(self, msg):
+        """
+        Construct the `HTTPHeaders` with all the necessary information for the
+        request.
+        """
+        # construct the headers
+        headers = HTTPHeaders()
+
+        if msg.curi.req_header:
+            # check if we have a previous Etag
+            if "Etag" in msg.curi.req_header:
+                headers["If-None-Match"] = \
+                    msg.curi.req_header["Etag"]
+
+
+        # manually set the Host header since we are requesting using an IP
+        host = urlsplit(msg.curi.url).hostname
+        if host is None:
+            LOG.error("proc.fetch::cannot extract hostname from url '%s'" %
+                    msg.curi.url)
+        else:
+            headers["Host"] = host
+
+        return headers
 
     def _handle_response(self, msg, out_socket):
         """
         Callback is being called when the data has been retrieved from the web.
         """
-        def extract_and_send_data(response):
-            msg.curi.content_body = response.body
+        def handle_server_response(response):
+            self._extract_info_from_response(response, msg)
+            LOG.info("proc.fetch::response for %s (took '%s'ms)" % (msg.curi.url,
+                        response.request_time))
             out_socket.send_multipart(msg.serialize())
 
-        return extract_and_send_data
+        return handle_server_response
+
+    def _extract_info_from_response(self, response, msg):
+        """
+        Extract the interesting information from a HTTPResponse.
+        """
+        msg.curi.status_code = response.code
+        msg.curi.content_body = response.body
+        msg.curi.req_header = response.request.headers
+        msg.curi.rep_header = response.headers
+        msg.curi.req_time = response.request_time
+        msg.curi.queue_time = response.time_info["queue"]
---
+++ test/test_fetch_processor_last_modified_works.py
@@ -0,0 +1,177 @@
+#
+# Copyright (c) 2010 Daniel Truemper truemped@googlemail.com
+#
+# test_fetch_processor_last_modified_works.py 17-Jan-2011
+#
+# All programs in this directory and
+# subdirectories are published under the GNU General Public License as
+# described below.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+#
+# Further information about the GNU GPL is available at:
+# http://www.gnu.org/copyleft/gpl.html
+#
+#
+
+import os.path
+import time
+import random
+
+import unittest
+
+import tornado
+import tornado.httpserver
+import tornado.web
+
+import zmq
+from zmq.eventloop.ioloop import IOLoop
+from zmq.eventloop.zmqstream import ZMQStream
+
+from spyder.core.constants import ZMQ_SPYDER_MGMT_WORKER
+from spyder.core.constants import ZMQ_SPYDER_MGMT_WORKER_QUIT
+from spyder.core.constants import ZMQ_SPYDER_MGMT_WORKER_QUIT_ACK
+from spyder.core.messages import DataMessage
+from spyder.core.mgmt import ZmqMgmt
+from spyder.core.worker import AsyncZmqWorker
+from spyder.core.settings import Settings
+from spyder.processor.fetcher import FetchProcessor
+from spyder.thrift.gen.ttypes import CrawlUri
+
+
+class ZmqWorkerIntegrationTestBase(unittest.TestCase):
+
+    def setUp(self):
+        
+        # create context
+        self._context = zmq.Context(1)
+
+        # create the ioloop
+        self._ioloop = IOLoop.instance()
+
+        t = time.time()
+
+        mgmt_master_worker = 'inproc://master/worker/coordination/%s' % t
+        mgmt_worker_master = 'inproc://worker/master/coordination/%s' % t
+
+        # create sockets
+        self._mgmt_sockets = dict()
+        self._mgmt_sockets['master_pub'] = self._context.socket(zmq.PUB)
+        self._mgmt_sockets['master_pub'].bind(mgmt_master_worker)
+
+        self._mgmt_sockets['worker_sub'] = self._context.socket(zmq.SUB)
+        self._mgmt_sockets['worker_sub'].connect(mgmt_master_worker)
+
+        self._mgmt_sockets['worker_pub'] = self._context.socket(zmq.PUB)
+        self._mgmt_sockets['worker_pub'].bind(mgmt_worker_master)
+
+        self._mgmt_sockets['master_sub'] = self._context.socket(zmq.SUB)
+        self._mgmt_sockets['master_sub'].connect(mgmt_worker_master)
+        self._mgmt_sockets['master_sub'].setsockopt(zmq.SUBSCRIBE, "")
+
+        data_master_worker = 'inproc://master/worker/pipeline/%s' % t
+        data_worker_master = 'inproc://worker/master/pipeline/%s' % t
+
+        self._worker_sockets = dict()
+        self._worker_sockets['master_push'] = self._context.socket(zmq.PUSH)
+        self._worker_sockets['master_push'].bind(data_master_worker)
+
+        self._worker_sockets['worker_pull'] = self._context.socket(zmq.PULL)
+        self._worker_sockets['worker_pull'].connect(data_master_worker)
+
+        self._worker_sockets['worker_pub'] = self._context.socket(zmq.PUB)
+        self._worker_sockets['worker_pub'].bind(data_worker_master)
+
+        self._worker_sockets['master_sub'] = self._context.socket(zmq.SUB)
+        self._worker_sockets['master_sub'].connect(data_worker_master)
+        self._worker_sockets['master_sub'].setsockopt(zmq.SUBSCRIBE, "")
+
+        self._mgmt = ZmqMgmt( self._mgmt_sockets['worker_sub'],
+            self._mgmt_sockets['worker_pub'], ioloop=self._ioloop)
+        self._mgmt.start()
+        self._mgmt.add_callback(ZMQ_SPYDER_MGMT_WORKER, self.on_mgmt_end)
+
+    def tearDown(self):
+
+        self._mgmt._stream.flush()
+        self._mgmt.stop()
+
+        for socket in self._mgmt_sockets.itervalues():
+            socket.close()
+
+        for socket in self._worker_sockets.itervalues():
+            socket.close()
+
+        self._context.term()
+
+    def on_mgmt_end(self, _msg):
+        self._ioloop.stop()
+
+
+class SimpleFetcherTestCase(ZmqWorkerIntegrationTestBase):
+
+    port = random.randint(8000, 9000)
+
+    def setUp(self):
+        ZmqWorkerIntegrationTestBase.setUp(self)
+
+        path = os.path.join(os.path.dirname(__file__), "static")
+        application = tornado.web.Application([
+            (r"/(.*)", tornado.web.StaticFileHandler, {"path": path}),
+        ])
+        self._server = tornado.httpserver.HTTPServer(application, io_loop =
+                self._ioloop)
+        self._server.listen(self.port)
+
+    def test_fetching_last_modified_works(self):
+
+        settings = Settings()
+        fetcher = FetchProcessor(settings, io_loop=self._ioloop)
+
+        worker = AsyncZmqWorker( self._worker_sockets['worker_pull'],
+            self._worker_sockets['worker_pub'],
+            self._mgmt,
+            fetcher,
+            self._ioloop)
+
+        curi = CrawlUri(url="http://localhost:%s/robots.txt" % self.port,
+                host_identifier="127.0.0.1",
+                effective_url="http://127.0.0.1:%s/robots.txt" % self.port,
+                req_header = { "Last-Modified" :
+                    "Mon, 17 Jan 2011 13:16:39 GMT" }
+                )
+
+        msg = DataMessage()
+        msg.identity = "me"
+        msg.curi = curi
+
+        self._worker_sockets['master_push'].send_multipart(msg.serialize())
+
+        def assert_expected_result_and_stop(raw_msg):
+            msg = DataMessage(raw_msg)
+            self.assertEqual(304, msg.curi.status_code)
+            self.assertEqual("", msg.curi.content_body)
+            self._mgmt_sockets['master_pub'].send_multipart(ZMQ_SPYDER_MGMT_WORKER_QUIT)
+
+        stream = ZMQStream(self._worker_sockets['master_sub'], self._ioloop)
+        stream.on_recv(assert_expected_result_and_stop)
+
+        worker.start()
+        self._ioloop.start()
+
+
+if __name__ == '__main__':
+    unittest.main()
