e5d792d8c950ab1e28b1630e1629525b46ce18fc
Daniel Truemper <truemper@retresco.de>
2011-02-04 16:09:22 +0100

    Generic logging mixin
    refs #7

---
+++ src/spyder/core/log.py
@@ -0,0 +1,43 @@
+#
+# Copyright (c) 2010 Daniel Truemper truemped@googlemail.com
+#
+# logging.py 04-Feb-2011
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# under the License.
+# All programs in this directory and
+# subdirectories are published under the GNU General Public License as
+# described below.
+#
+#
+"""
+A simple pyzmq logging mixin.
+"""
+
+import logging
+
+from zmq.log.handlers import PUBHandler
+
+
+class LoggingMixin:
+    """
+    Simple mixin for adding logging methods to a class.
+    """
+
+    def __init__(self, pub_handler, log_level):
+        """
+        Initialize the logger.
+        """
+        self._logger = logging.getLogger()
+        self._logger.addHandler(pub_handler)
+        self._logger.setLevel(log_level)
