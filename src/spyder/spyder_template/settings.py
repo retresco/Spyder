34b73d466a683b757cf5bf3dbdfd80fcf9e08c10
Daniel Truemper <truemped@googlemail.com>
2011-04-21 20:03:22 +0200

    Master and sink in the template

--- src/spyder/spyder_template/settings.py
+++ src/spyder/spyder_template/settings.py
@@ -1,23 +1,26 @@
 #
-# Copyright (c) 2011 Daniel Truemper truemped@googlemail.com
-#
-# settings.py 02-Feb-2011
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# settings.py
 #
 """
-Your application specific settings.
+Your crawler specific settings.
 """
 import logging
 
-LOG_LEVEL = logging.ERROR
+LOG_LEVEL_MASTER = logging.INFO
+LOG_LEVEL_WORKER = logging.INFO
+
+USER_AGENT = "Mozilla/5.0 (compatible; spyder/0.1; " + \
+    "+http://github.com/retresco/spyder)"
+
+# callback for initializing the periodic crawling of the sitemap
+MASTER_CALLBACK = 'master.initialize'
+
+# List of positive regular expressions for the crawl scope
+REGEX_SCOPE_POSITIVE = [
+    "^http://www\.dmoz\.org/Recreation/Boating/Sailing/.*",
+]
+
+# List of negative regular expressions for the crawl scope
+REGEX_SCOPE_NEGATIVE = [
+    "^http://www\.dmoz\.org/Recreation/Boating/Sailing/Racing/.*",
+]
