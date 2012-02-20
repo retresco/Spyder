899281c73f0f197ef40db223532a0c4bd05d9dcc
Norman Rosner <norman@rosner.io>
2012-02-20 21:01:15 +0100

    Changes default value of proxy host.

--- src/spyder/defaultsettings.py
+++ src/spyder/defaultsettings.py
@@ -44,7 +44,7 @@ USE_GZIP = True
 
 # Proxy configuration. Both PROXY_HOST and PROXY_PORT must be set! 
 # PROXY_USERNAME and PROXY_PASSWORD are optional
-PROXY_HOST = ''
+PROXY_HOST = None
 PROXY_PORT = None
 PROXY_USERNAME = ''
 PROXY_PASSWORD = ''
